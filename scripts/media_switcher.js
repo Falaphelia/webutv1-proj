ce98815d40076e2c9dc525cb2cbfc06a933878b8
Falaphelia <simonkarlsson797@gmail.com>
2026-05-29 21:23:34 +0200

    fix alt

--- scripts/media_switcher.js
+++ scripts/media_switcher.js
@@ -2,43 +2,64 @@ const currentIndices = {};
 
 const projectData = {
     "plane": {
+        alt: "Images showing different angles of a custom-built airplane in the game: Stormworks",
         files: [
-            "assets/creations/plane/show.avif",
-            "assets/creations/plane/flight.avif",
-            "assets/creations/plane/cockpit.avif",
-            "assets/creations/plane/night.avif",
-            "assets/creations/plane/engine.mp3"
-        ],
-        descriptions: [
-            "This is one of my first functional planes, well still my first functional one...",
-            "Gotta enjoy the view",
-            "Yeah I love buttons, do you like buttons?",
-            "Sure is dark here! Maybe you can see something if you pay attention?",
-            "Hear the engine run!. You can click here on the text to switch again!"
+            {
+                src: "assets/creations/plane/show.avif",
+                description: "This is one of my first functional planes, well still my first functional one..."
+            },
+            {
+                src: "assets/creations/plane/flight.avif",
+                description: "Gotta enjoy the view"
+            },
+            {
+                src: "assets/creations/plane/cockpit.avif",
+                description: "Yeah I love buttons, do you like buttons?"
+            },
+            {
+                src: "assets/creations/plane/night.avif",
+                description: "Sure is dark here! Maybe you can see something if you pay attention?"
+            },
+            {
+                src: "assets/creations/plane/engine.mp3",
+                description: "Hear the engine run! You can click here on the text to switch again!"
+            }
         ]
     },
 
     "tracked-L1": {
-        files: ["assets/creations/tracked-L1/show.avif",
-            "assets/creations/tracked-L1/side.avif",
-            "assets/creations/tracked-L1/driver.avif",
-            "assets/creations/tracked-L1/running.mp4"],
-        descriptions: [
-            "My first tracked vehicle!",
-            "Surely it looks good, right? I'd say it's pretty fast for a tracked vehicle!",
-            "Nice and simple cockpit... I learned something from the plane I think.",
-            "Look at it driving! You can click here on the text to switch again!"
+        alt: "Showing my tracked search and rescue vehicle from different angles- including the cockpit!",
+        files: [
+            {
+                src: "assets/creations/tracked-L1/show.avif",
+                description: "My first tracked vehicle!"
+            },
+            {
+                src: "assets/creations/tracked-L1/side.avif",
+                description: "Surely it looks good, right? I'd say it's pretty fast for a tracked vehicle!"
+            },
+            {
+                src: "assets/creations/tracked-L1/driver.avif",
+                description: "Nice and simple cockpit... I learned something from the plane I think."
+            },
+            {
+                src: "assets/creations/tracked-L1/running.mp4",
+                description: "Look at it driving! You can click here on the text to switch again!"
+            }
         ]
     },
 
     "career-boat": {
-        files: ["assets/creations/boat/show.avif",
-            "assets/creations/boat/side.avif",],
-        descriptions: [
-            "My first tracked vehicle!",
-            "Surely it looks good, right? I'd say it's pretty fast for a tracked vehicle!",
-            "Nice and simple cockpit... I learned something from the plane I think.",
-            "Look at it driving! You can click here on the text to switch again!"
+        alt: "A showcase of one of my first functioning vehicles, perhaps a bit outdated now though",
+        files: [
+            {
+                src: "assets/creations/boat/show.avif",
+                description: "My first boat build!"
+            },
+            {
+                src: "assets/creations/boat/side.avif",
+                description: "Simple and clean design."
+            }
         ]
     }
 };
@@ -59,7 +80,6 @@ function media_switcher(side, projectName, containerId) {
              style="display: flex; flex-direction: ${side === 'left' ? 'row' : 'row-reverse'}">
 
             <div class="media-display" style="flex: 1;"></div>
-
             <div class="media-description" style="flex: 1;"></div>
 
         </div>
@@ -68,16 +88,17 @@ function media_switcher(side, projectName, containerId) {
     function updateView() {
 
         const index = currentIndices[containerId];
+        const item = projectData[projectName].files[index];
 
-        const file = projectData[projectName].files[index];
-        const description = projectData[projectName].descriptions[index];
+        const file = item.src;
+        const description = item.description;
+        const alt = projectData[projectName].alt;
 
         const display = container.querySelector('.media-display');
         const descriptionBox = container.querySelector('.media-description');
 
         descriptionBox.textContent = description;
 
-        /* Make sure to render using correct html tag */
         if (file.endsWith('.mp4')) {
 
             display.innerHTML = `
@@ -93,7 +114,7 @@ function media_switcher(side, projectName, containerId) {
         } else {
 
             display.innerHTML = `
-                <img src="${file}" width="100%">
+                <img src="${file}" alt="${alt}" width="100%">
             `;
         }
     }
